1379737337d76e1abcd21ebd338f9586119264e6
Evan Thibodeau <evanthibodeau@Evans-MacBook-Pro.local>
2019-08-28 13:53:19 +0400

    Add other fragments and SF

---
+++ src/clay/carousel/main.js
@@ -0,0 +1,28 @@
+var carousel = $('.carousel');
+
+console.log(window);
+
+if (!window.__carousel_registered) {
+	$(fragmentElement).on(
+		'click', 
+		'.play-pause',
+		{},
+		function(event) {
+			console.log(event);
+
+			var pause = $(event.currentTarget);
+
+			if (pause.hasClass('paused')) {
+				carousel.carousel('next');
+				carousel.carousel('cycle');
+			}
+			else {
+				carousel.carousel('pause');
+			}
+
+			pause.toggleClass('paused');
+		}
+	);
+}
+
+window.__carousel_registered = true;
